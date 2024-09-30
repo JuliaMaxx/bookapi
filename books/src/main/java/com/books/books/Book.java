458dbae37cd435ba21d90ccf781c30800606a6b3
Julia Max <jjuliamaxxx@gmail.com>
2024-09-30 13:07:18 +0200

    feat: create Book Entity

---
+++ books/src/main/java/com/books/books/Book.java
@@ -0,0 +1,64 @@
+package com.books.books;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int year;
+    private int rating;
+
+    public Book() {}
+
+    public Book(String title, String author, int year, int rating) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
