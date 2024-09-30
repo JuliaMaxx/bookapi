c76071a0cc9d75f30a67b2ef0577c97002b5bcc7
Julia Max <jjuliamaxxx@gmail.com>
2024-09-30 13:53:59 +0200

    feat: add mock data

--- books/src/main/java/com/books/books/BooksApplication.java
+++ books/src/main/java/com/books/books/BooksApplication.java
@@ -1,13 +1,26 @@
 package com.books.books;
 
+import com.books.books.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(BookRepository bookRepository) {
+		return args -> {
+			bookRepository.save(new Book("IT", "Stephen King", 1986, 5));
+			bookRepository.save(new Book("Forest Gump", "Winston Groom", 1986, 4));
+			bookRepository.save(new Book("1984", "George Orwell", 1949, 3));
+			bookRepository.save(new Book("The Shining", "Stephen King", 1977, 3));
+		};
 	}
 
 }
