beedc7a8080bf215f07a2b8c96c0595c4648e117
Julia Max <jjuliamaxxx@gmail.com>
2024-09-30 13:20:19 +0200

    feat: create repository interface

---
+++ books/src/main/java/com/books/books/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.books.books.repository;
+
+import com.books.books.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContaining(String title);
+    List<Book> findByAuthor(String author);
+    List<Book> findByYear(int year);
+    List<Book> findByRating(int rating);
+}
